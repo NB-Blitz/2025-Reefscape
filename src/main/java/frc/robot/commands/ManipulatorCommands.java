30147dd6141787b3dd992bfe7e154df568cb6880
Hex <theamazingmike111@gmail.com>
2025-02-03 20:17:38 -0600

    Latest Manipulator Button mapping

---
+++ src/main/java/frc/robot/commands/ManipulatorCommands.java
@@ -0,0 +1,23 @@
+package frc.robot.commands;
+import java.util.function.DoubleSupplier;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.manipulator.Manipulator;
+
+public class ManipulatorCommands {
+
+    public ManipulatorCommands(){}
+    public static Command joystickManipulator(
+      Manipulator manipulator,
+      DoubleSupplier leftYSupplier, 
+      DoubleSupplier rightYSupplier) {
+    return Commands.run(
+        () -> {
+            manipulator.runManipulator(leftYSupplier.getAsDouble(), rightYSupplier.getAsDouble());
+        }    
+    );
+
+}
+
+
+}
